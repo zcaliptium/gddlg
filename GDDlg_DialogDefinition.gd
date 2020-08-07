e79bd7c0c81cc45c89ad565e01288e97b4e174e3
zcaliptium <zcaliptium@gmail.com>
2020-08-07 11:33:20 +0300

    Add from_data/to_data methods for dialog definition.
    That is just draft because there is no checks for loaded data.

--- GDDlg_DialogDefinition.gd
+++ GDDlg_DialogDefinition.gd
@@ -13,3 +13,30 @@ func _init(id: String) -> void:
 
 func get_attribute(key: String, default = null):
 	return attributes.get(key, default);
+
+# Get stack data from the Dictionary.
+#   For example you can get such dictionary from JSON.
+func from_data(json_data: Dictionary):
+	var item_id = json_data.get("id", "null");
+	var item_attributes = json_data.get("attributes", {});
+	var item_options = json_data.get("options", []);
+	
+	if (typeof(item_id) == TYPE_STRING):
+		identifier = item_id;
+		
+	if (typeof(item_attributes) == TYPE_DICTIONARY):
+		attributes = item_attributes;
+
+	if (typeof(item_options) == TYPE_ARRAY):
+		options = item_options;
+
+# Returns Dictionary that represents this dialog definition.
+#   Use to_json on result to get JSON string.
+func to_data() -> Dictionary:
+	var data: Dictionary = {};
+	
+	data["id"] = identifier;
+	data["attributes"] = attributes;
+	data["options"] = options;
+	
+	return data;
